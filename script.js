51fefa8cf13728d71fa8eaa1631145f0e15325f0
“darek632” <162553961+darek632@users.noreply.github.com>
2024-07-03 14:29:34 +0200

    Add working display for number buttons
    Using click handler, clicked value displayed and
    stored in a variable for future use.

--- script.js
+++ script.js
@@ -42,6 +42,92 @@ function operate(val1,oper,val2) {
 
  console.log(test);
 
- 
+ // button function pseudo
+
+ //when button pressed 
+ // if display showing 0 then clear
+ //enter number into display
+ // store number in variable for future use
+
+ let displayValue;
+
+
+ let nine = document.querySelector(".nine");
+
+ let calculator = document.querySelector(".calculator");
+
+calculator.addEventListener("click", function (event) { 
+const display = document.querySelector(".display");
+let target = event.target;
+
+function updateDisplay(number) {
+    if (display.textContent === "0") {
+        display.textContent = number;
+    } else {
+        display.textContent += number;
+    }
+} // helper to keep common logic outside of switch cases
+
+
+switch (target.className) {
+    case "nine": 
+       updateDisplay("9");
+       displayValue = 9;
+       console.log(displayValue);
+    break;
+
+    case "eight": 
+        updateDisplay("8");
+        displayValue = 8;
+        console.log(displayValue);
+    break; 
+
+    case "seven": 
+       updateDisplay("7");
+       displayValue = 7;
+       console.log(displayValue);
+    break;
+
+    case "six": 
+       updateDisplay("6");
+       displayValue = 6;
+       console.log(displayValue);
+    break;
+
+    case "five": 
+       updateDisplay("5");
+       displayValue = 5;
+       console.log(displayValue);
+    break;
+
+    case "four": 
+       updateDisplay("4");
+       displayValue = 4;
+       console.log(displayValue);
+    break;
+
+    case "three": 
+       updateDisplay("3");
+       displayValue = 3;
+       console.log(displayValue);
+    break;
+
+    case "two": 
+       updateDisplay("2");
+       displayValue = 2;
+       console.log(displayValue);
+    break;
+
+    case "one": 
+       updateDisplay("1");
+       displayValue = 1;
+       console.log(displayValue);
+    break;
+
+ }
+
+});
+
+
 
 
